a9618af2d858738bf95e819876d68d71427e4975
delaram-golampoor-sagha <majesticdelaram@yahoo.com>
2023-01-28 18:41:46 +0330

    queue_using_a_linkedList

---
+++ queue/queue_using_a_linkedList/main.go
@@ -0,0 +1,74 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	value int
+	next  *Node
+}
+
+type QueueLinkedList struct {
+	head *Node
+	tail *Node
+	size int
+}
+
+func (q *QueueLinkedList) Size() int {
+	return q.size
+}
+
+func (q *QueueLinkedList) IsEmpty() bool {
+	return q.size == 0
+}
+
+func (q *QueueLinkedList) Peek() int {
+	if q.IsEmpty() {
+		fmt.Println("QueueEmptyException")
+		return 0
+	}
+	return q.head.value
+}
+
+func (q *QueueLinkedList) Add(value int) {
+	temp := &Node{value, nil}
+	if q.head == nil {
+		q.head = temp
+		q.tail = temp
+	} else {
+		q.tail.next = temp
+		q.tail = temp
+	}
+	q.size++
+}
+
+func (q *QueueLinkedList) Remove() int {
+	if q.IsEmpty() {
+		fmt.Println("QueueEmptyException")
+		return 0
+	}
+	value := q.head.value
+	q.head = q.head.next
+	q.size--
+	return value
+}
+
+func (q *QueueLinkedList) Print() {
+	temp := q.head
+	for temp != nil {
+		fmt.Print(temp.value, " ")
+		temp = temp.next
+	}
+	fmt.Println()
+}
+
+// Testing Code
+func main() {
+	q := new(QueueLinkedList)
+	q.Add(1)
+	q.Add(2)
+	q.Add(3)
+	for q.IsEmpty() == false {
+		val := q.Remove()
+		fmt.Println(val, " ")
+	}
+}
