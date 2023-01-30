94ec257002913b1ca5a9d590dce39e2e6455eb67
delaram-golampoor-sagha <majesticdelaram@yahoo.com>
2023-01-30 10:17:10 +0330

    nth_post_order

---
+++ Tree/nth_post_order/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func (t *Tree) NthPostOrder(index int) {
+	var counter int
+	nthPostOrder(t.root, index, &counter)
+}
+
+func nthPostOrder(node *Node, index int, counter *int) {
+	if node != nil {
+		nthPostOrder(node.left, index, counter)
+		nthPostOrder(node.right, index, counter)
+		(*counter)++
+		if *counter == index {
+			fmt.Println("NthPostOrder at index ", index, "is :", node.value)
+		}
+	}
+}
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	t := LevelOrderBinaryTree(arr)
+	t.NthPostOrder(5)
+}
